f6081621fc0da6985dd567b70bc24b5c3a6f5459
Luiz Gustavo <gustavospenezanni@gmail.com>
2019-08-13 15:32:06 -0300

    Inicial Commit

---
+++ src/main/java/br/com/spring/dev/DevApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+}
